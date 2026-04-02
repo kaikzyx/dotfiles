c8a77b5e22d3b092340c606bc55f6c4ef3a4a63c
Kaique Silva <kaikzyx@gmail.com>
2026-04-02 15:30:11 -0300

    refactor(nvim): remove unused neopywal theme plugin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,8 @@
 return {
   {
-    "RedsXDD/neopywal.nvim",
-    name = "neopywal",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-
-  {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
